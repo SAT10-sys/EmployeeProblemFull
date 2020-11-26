24612c2e027d0757f7f6fc9e0208b7cb03d9b482
Satantra Tewari <satantra1508@gmail.com>
2020-11-26 23:10:17 +0530

    [satantra] refactor. added class and methods to compute wages

---
+++ EmployeeProblemFull/EmployeeOperations.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeProblemFull
+{
+    public class EmployeeOperations
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int WAGE_PER_HOUR = 20;
+        public const int WORKING_DAYS_PER_MONTH = 20;
+        public const int TOTAL_WORKING_HOURS = 100;     
+        public int GetEmployeeHours()
+        {
+            Random random = new Random();
+            int i = 0;
+            int totalEmpHours = 0;
+            int empHours = 0;
+            while(i<WORKING_DAYS_PER_MONTH && totalEmpHours<TOTAL_WORKING_HOURS)
+            {
+                int empCheck = random.Next(0, 3);
+                switch(empCheck)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Absent");
+                        empHours = 0;
+                        break;
+                    case IS_PART_TIME:
+                        Console.WriteLine("Part time employee");
+                        empHours = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        Console.WriteLine("Full time employee");
+                        empHours = 8;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Result");
+                        break;
+                }
+                i++;
+                totalEmpHours = totalEmpHours + empHours;                
+            }
+            return totalEmpHours;
+        }
+        public void ComputeWage(int totalEmpHours)
+        {
+            int empWage = 0;
+            empWage = totalEmpHours * WAGE_PER_HOUR;
+            Console.WriteLine("Total hours worked: "+totalEmpHours+"\nTotal wage for the month: "+empWage);
+        }
+    }
+}
--- EmployeeProblemFull/Program.cs
+++ EmployeeProblemFull/Program.cs
@@ -7,72 +7,10 @@ namespace EmployeeProblemFull
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Management Problem");
-            const int IS_FULL_TIME = 2;
-            const int IS_PART_TIME = 1;
-            const int WAGE_PER_HOUR=20;
-            const int NUMBER_OF_WORKING_DAYS_PER_MONTH = 20;
-            const int MAXIMUM_WORKING_HOURS = 100;
-            Random random = new Random();            
-            int empHours = 0;
-            int empWage = 0;
-            int totalEmpHrs = 0;
-            int i = 0;
-            /*
-            // UC1_Checking for attendance
-            if(empCheck==IS_FULL_TIME)
-                Console.WriteLine("Employee is Present");
-            else
-                Console.WriteLine("Employee is Absent");            
-            //UC2 Calculating wage for part time and full time employee
-            if (empCheck == IS_FULL_TIME)
-                empHours = 8;
-            else
-                empHours = 0;
-            //UC3 Added Part time employee details
-            if (empHours == IS_FULL_TIME)
-            {
-                Console.WriteLine("Full time Employee");
-                empHours = 8;
-            }
-            else if (empHours == IS_PART_TIME)
-            {
-                Console.WriteLine("Part time Employee");
-                empHours = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHours = 0;
-            }*/
-
-            //UC 5 calculate monthly wages
-            while(i<NUMBER_OF_WORKING_DAYS_PER_MONTH && totalEmpHrs<MAXIMUM_WORKING_HOURS)
-            {
-                int empCheck = random.Next(0, 3); //generates a random number between 0 and 3 where 0 is inclusive but 2 is exclusive
-                //UC4 Implemented Using Switch Case
-                switch (empCheck)
-                {
-                    case 0:
-                        Console.WriteLine("Employee is absent");
-                        empHours = 0;
-                        break;
-                    case IS_PART_TIME:
-                        Console.WriteLine("Part Time Employee");
-                        empHours = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        Console.WriteLine("Full Time Employee");
-                        empHours = 8;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Entry");
-                        break;
-                }
-                i++;
-                totalEmpHrs = totalEmpHrs + empHours;
-            }
-            empWage = totalEmpHrs * WAGE_PER_HOUR;
-            Console.WriteLine("Total hours worked: "+totalEmpHrs+"\nMonthly wages: " + empWage);
+            EmployeeOperations employeeOperations = new EmployeeOperations();
+            int totEmpHrs = employeeOperations.GetEmployeeHours();
+            employeeOperations.ComputeWage(totEmpHrs);
         }
     }
 }
+           
