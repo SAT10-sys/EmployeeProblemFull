e06f343fe8d0e27d4665666fe50a6bcdeabfb31f
Satantra Tewari <satantra1508@gmail.com>
2020-11-26 22:27:13 +0530

    [satantra] checked for attendance

--- EmployeeProblemFull/Program.cs
+++ EmployeeProblemFull/Program.cs
@@ -7,6 +7,15 @@ namespace EmployeeProblemFull
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Management Problem");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2); //generates a random number between 0 and 2 where 0 is inclusive but 2 is exclusive
+
+            // UC1_Checking for attendance
+            if(empCheck==IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
