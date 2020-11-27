89801510608ebc3ed838a937aad63103de89fab4
Satantra Tewari <satantra1508@gmail.com>
2020-11-27 20:18:26 +0530

    [satantra] test case passed for adding employee

--- EmployeeTest/UnitTest1.cs
+++ EmployeeTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EmployeeProblemFull;
+using System;
 
 namespace EmployeeTest
 {
@@ -15,5 +16,24 @@ namespace EmployeeTest
             bool result = employeeRepo.UpdateSalary(name, salary);
             Assert.AreEqual(result, true);
         }       
+        [TestMethod]
+        public void OnaddingEmployeeShouldSyncWithDB()
+        {
+            EmployeeDetails employee = new EmployeeDetails();
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            employee.EmployeeName = "Harry";
+            employee.Department = "Finance";
+            employee.PhoneNumber = "9999999999";
+            employee.Address = "Kolkata";
+            employee.Gender = Convert.ToChar("M");
+            employee.StartDate = DateTime.Today;
+            employee.BasicPay = Convert.ToDecimal(800000);
+            employee.Deductions = Convert.ToDecimal(80000);
+            employee.TaxablePay = Convert.ToDecimal(720000);
+            employee.IncomeTax = Convert.ToDecimal(7200);
+            employee.NetPay = Convert.ToDecimal(712800);
+            bool result = employeeRepo.AddEmployee(employee);
+            Assert.AreEqual(result, true);
+        }
     }
 }
